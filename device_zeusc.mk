a85002821e3d523ee5004bd474ce432ae790297c
jerpelea <jerpelea@gmail.com>
2011-09-23 13:29:51 +0300

    semc:initial push for zeusc device

---
+++ device_zeusc.mk
@@ -0,0 +1,118 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/semc/zeusc/zeusc-vendor.mk)
+
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := zeusc
+PRODUCT_DEVICE := zeusc
+PRODUCT_MODEL := zeusc
+
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/semc/zeusc/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+-include device/semc/msm7x30-common/msm7x30.mk
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+DISABLE_DEXPREOPT := false
+
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
+DEVICE_PACKAGE_OVERLAYS += device/semc/zeusc/overlay
+
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES += \
+	device/semc/zeusc/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x30-common/prebuilt/ueventd.zeus.rc:root/ueventd.zeus.rc \
+    device/semc/msm7x30-common/prebuilt/init.zeus.rc:root/init.semc.rc \
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
+    device/semc/zeus/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    device/semc/zeus/prebuilt/bootrec:root/sbin/bootrec
+
+#recovery resources
+PRODUCT_COPY_FILES += \
+    bootable/recovery/res/images/icon_clockwork.png:root/res/images/icon_clockwork.png \
+    bootable/recovery/res/images/icon_error.png:root/res/images/icon_error.png \
+    bootable/recovery/res/images/icon_installing.png:root/res/images/icon_installing.png \
+    bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate1.png \
+    bootable/recovery/res/images/indeterminate2.png:root/res/images/indeterminate2.png \
+    bootable/recovery/res/images/indeterminate3.png:root/res/images/indeterminate3.png \
+    bootable/recovery/res/images/indeterminate4.png:root/res/images/indeterminate4.png \
+    bootable/recovery/res/images/indeterminate5.png:root/res/images/indeterminate5.png \
+    bootable/recovery/res/images/indeterminate6.png:root/res/images/indeterminate6.png \
+    bootable/recovery/res/images/progress_empty.png:root/res/images/progress_empty.png \
+    bootable/recovery/res/images/progress_fill.png:root/res/images/progress_fill.png
+
+#WIFI modules and configs
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x30-common/modules/bcm4329.ko:root/modules/bcm4329.ko
+
+# semc msm7x30 uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+#Offline charging animation
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_01_H.png:system/semc/chargemon/data/charging_animation_01.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_02_H.png:system/semc/chargemon/data/charging_animation_02.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_03_H.png:system/semc/chargemon/data/charging_animation_03.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_H.png:system/semc/chargemon/data/charging_animation_04.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_H.png:system/semc/chargemon/data/charging_animation_05.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_H.png:system/semc/chargemon/data/charging_animation_06.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_blank_H.png:system/semc/chargemon/data/charging_animation_blank.png
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-qc-1.so \
+    rild.libargs=-d /dev/smd0 \
+    persist.rild.nitz_plmn= \
+    persist.rild.nitz_long_ons_0= \
+    persist.rild.nitz_long_ons_1= \
+    persist.rild.nitz_long_ons_2= \
+    persist.rild.nitz_long_ons_3= \
+    persist.rild.nitz_short_ons_0= \
+    persist.rild.nitz_short_ons_1= \
+    persist.rild.nitz_short_ons_2= \
+    persist.rild.nitz_short_ons_3= \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+    ro.telephony.default_network=4 \
+    ro.telephony.call_ring.multiple=false \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=15 \
+    ro.sf.lcd_density=240 \
+    keyguard.no_require_sim=true \
+    ro.com.google.locationfeatures=1 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.heapsize=32m \
+    dalvik.vm.dexopt-data-only=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.checkjni=false \
+    ro.kernel.android.checkjni=0 \
+    ro.opengles.version=131072  \
+    ro.compcache.default=0 \
+    ro.product.locale.language=en \
+    ro.product.locale.region=US \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.cdma.home.operator.numeric = 310012 \
+    ro.cdma.home.operator.alpha = Verizon \
+    ro.cdma.data_retry_config = max_retries=infinite,0,0,60000,120000,480000,900000 \
+    persist.telephony.support_ipv6=true \
+    persist.telephony.support_ipv4=true \
+    ro.ril.vzw.feature = 1 \
+    ro.ril.enable.managed.roaming = 1 \
+    ro.ril.wp.feature = 1 \
+    BUILD_UTC_DATE=0
